03a64880e0e43721efce08a7dc83ed7c87b9e5c2
Sergio-Mira <sergio.mira@swrve.com>
2015-07-14 16:16:46 +0100

    Only get trackingId when needed

--- AdobeMobileServices/Android/SwrveAdobeMobileServicesDemo/app/src/main/java/swrveadbmobile/swrve/com/swrveadobemobileservicesdemo/SwrveAnalytics.java
+++ AdobeMobileServices/Android/SwrveAdobeMobileServicesDemo/app/src/main/java/swrveadbmobile/swrve/com/swrveadobemobileservicesdemo/SwrveAnalytics.java
@@ -3,7 +3,6 @@ package swrveadbmobile.swrve.com.swrveadobemobileservicesdemo;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import com.adobe.mobile.Analytics;
 import com.swrve.sdk.SwrveHelper;
@@ -37,13 +36,12 @@ public class SwrveAnalytics {
     }
 
     public static void createInstance(Application app, int appId, String apiKey, SwrveConfig config) {
-        String trackingId = Analytics.getTrackingIdentifier();
         if (config == null) {
             config = new SwrveConfig();
         }
         // Use the ADBMobile trackingId as user if if none was configured
         if (SwrveHelper.isNullOrEmpty(config.getUserId())) {
-            config.setUserId(trackingId);
+            config.setUserId(Analytics.getTrackingIdentifier());
         }
         SwrveSDK.createInstance(app, appId, apiKey, config);
     }
