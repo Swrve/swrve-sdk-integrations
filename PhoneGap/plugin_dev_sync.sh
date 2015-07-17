c6f554ccc20c99f7ca68865d3f91784cc1cc6109
Sergio Mira <sergio.mira@swrve.com>
2015-07-17 16:33:52 +0100

    Update plugin_dev_sync.sh

--- PhoneGap/plugin_dev_sync.sh
+++ PhoneGap/plugin_dev_sync.sh
@@ -5,15 +5,15 @@ set -e
 PLUGIN="SwrvePlugin"
 DEMO="SwrvePhoneGapDemo"
 DEMOIOS="SwrvePhoneGapDemo/platforms/ios"
-DEMOIANDROID="SwrvePhoneGapDemo/platforms/android"
+DEMOANDROID="SwrvePhoneGapDemo/platforms/android"
 
 # Global plugin
 cp -R $PLUGIN/ $DEMO/com.swrve.SwrvePlugin/
 
 # Android platform
-cp $PLUGIN/platforms/android/build.gradle $DEMOIANDROID/com.swrve.SwrvePlugin/helloworld-build.gradle
-cp $PLUGIN/platforms/android/src/com/swrve/SwrvePlugin.java $DEMOIANDROID/src/com/swrve/SwrvePlugin.java
-cp -R $PLUGIN/js/swrve-android.js $DEMOIANDROID/assets/www/plugins/com.swrve.SwrvePlugin/js/swrve-android.js
+cp $PLUGIN/platforms/android/build.gradle $DEMOANDROID/com.swrve.SwrvePlugin/helloworld-build.gradle
+cp $PLUGIN/platforms/android/src/com/swrve/SwrvePlugin.java $DEMOANDROID/src/com/swrve/SwrvePlugin.java
+cp -R $PLUGIN/js/swrve-android.js $DEMOANDROID/assets/www/plugins/com.swrve.SwrvePlugin/js/swrve-android.js
 
 # iOS platform
 cp -R $PLUGIN/platforms/ios/ $DEMOIOS/Hello\ World/Plugins/com.swrve.SwrvePlugin/
